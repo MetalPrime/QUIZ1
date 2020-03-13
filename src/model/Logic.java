2ebc2445d80c1b5b575faac0b4c10fc5c554d19b
Jhon Garcia <metalprime57@gmail.com>
2020-03-13 15:56:58 -0500

    add paramets

--- src/model/Logic.java
+++ src/model/Logic.java
@@ -8,20 +8,47 @@ public class Logic {
 
 	private PApplet app;
 	private String[] txtload; 
+	private String[] dataCircle;
+	private String[] dataRect;
 	private ArrayList <Circle> circle;
 	private ArrayList <Rect> rect;
 	
 	public Logic(PApplet app) {
 		this.app = app;
+		
 		txtload = app.loadStrings("./../data/text.txt");
-		System.out.println(txtload.length);
+		circle = new ArrayList <Circle>();
+		rect = new ArrayList<Rect>();
+	
 		for(int i=0; i<txtload.length;i++) {
+			//System.out.println(txtload[i]);
 			if(txtload[i].contains("Circulo")) {
-				circle.add(new Circle(i, i, i, i));
+				
+				dataCircle = txtload[i].split(" ");
+				for (int j = 0; j < dataCircle.length; j++) {
+					System.out.println(dataCircle[j]);
+					int size = Integer.parseInt(dataCircle[0]);
+					int posX = Integer.parseInt(dataCircle[1]);
+					int posY = Integer.parseInt(dataCircle[2]);
+					int dir = Integer.parseInt(dataCircle[3]);
+					circle.add(new Circle(size, posX, posY, dir));
+				}
+				
 			}
 			if(txtload[i].contains("Cuadrado")) {
-				rect.add(new Rect(i, i, i, i));
+				
+				dataRect = txtload[i].split(" ");
+				for (int j = 0; j < dataRect.length; j++) {
+					System.out.println(dataRect[j]);
+					int size = Integer.parseInt(dataCircle[0]);
+					int posX = Integer.parseInt(dataCircle[1]);
+					int posY = Integer.parseInt(dataCircle[2]);
+					int dir = Integer.parseInt(dataCircle[3]);
+					rect.add(new Rect(size, posX, posY, dir));
+				}
+				
 			}
+		//System.out.println(circle.size());
 		}
 		
 	}
@@ -30,7 +57,13 @@ public class Logic {
 
 	public void paint() {
 		// TODO Auto-generated method stub
+		for (int i = 0; i < rect.size(); i++) {
+			rect.get(i).paint();
+		}
 		
+		for (int i = 0; i < circle.size(); i++) {
+			circle.get(i).paint();
+		}
 	}
 
 	public void move() {
