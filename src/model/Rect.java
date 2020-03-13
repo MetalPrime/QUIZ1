132cfeace895758dc7701509120200c59ee1af8e
Jhon Garcia <metalprime57@gmail.com>
2020-03-13 15:14:25 -0500

    update rect

--- src/model/Rect.java
+++ src/model/Rect.java
@@ -1,5 +1,26 @@
 package model;
 
-public class Rect {
+public class Rect extends Figure {
+
+	public Rect(int size, float posX, float posY, float dir) {
+		super(size, posX, posY, dir);
+		// TODO Auto-generated constructor stub
+		this.size = size;
+		this.posX = posX;
+		this.posY = posY;
+		this.dir = dir;
+	}
+
+	@Override
+	public void paint() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void move() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
