e777edb3c5152c7e0e9175b8a5b4e5571f3a82cc
Jhon Garcia <metalprime57@gmail.com>
2020-03-13 15:25:28 -0500

    add elements resting

--- src/model/Logic.java
+++ src/model/Logic.java
@@ -1,14 +1,32 @@
 package model;
 
+import java.util.ArrayList;
+
 import processing.core.PApplet;
 
 public class Logic {
 
 	private PApplet app;
+	private String[] txtload; 
+	private ArrayList <Circle> circle;
+	private ArrayList <Rect> rect;
 	
 	public Logic(PApplet app) {
 		this.app = app;
+		txtload = app.loadStrings("./../data/text.txt");
+		System.out.println(txtload.length);
+		for(int i=0; i<txtload.length;i++) {
+			if(txtload[i].contains("Circulo")) {
+				circle.add(new Circle(i, i, i, i));
+			}
+			if(txtload[i].contains("Cuadrado")) {
+				rect.add(new Rect(i, i, i, i));
+			}
+		}
+		
 	}
+
+	
 
 	public void paint() {
 		// TODO Auto-generated method stub
